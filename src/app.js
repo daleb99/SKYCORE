a43f7b15135f0f4f380d22e1ed5145e85c38e475
Dale <dale.brooksby@gmail.com>
2021-06-12 00:22:14 +0100

    Turn on express.static for /src

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const sockets = require('./socketServer/socket-server');
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static('public'));
+app.use(express.static('src'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
